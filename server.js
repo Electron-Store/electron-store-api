8d0a180f053471dea91abc6580b47a32b1f22ff3
PATRICK <pntxall100@gmail.com>
2021-08-11 18:24:19 +0300

    Fixed cors

--- server.js
+++ server.js
@@ -1,43 +1,45 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
+
+const cParser = require("./categoriesParser");
+const services = require("./services");
+const appParser = require("./appParser");
 
 const app = express();
+app.use(cors());
 
-const cParser = require('./categoriesParser')
-const services = require('./services')
-const appParser = require('./appParser')
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000
-
-app.use(express.json())
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log("Server running");
-})
+	console.log("Server running");
+});
 
-app.get('/refresh-db', (req, res) => {
-    cParser.getApps()
-    res.send({ msg: "Refreshing apps DB" })
-})
+app.get("/refresh-db", (req, res) => {
+	cParser.getApps();
+	res.send({ msg: "Refreshing apps DB" });
+});
 
-app.get('/get-app', async (req, res) => {
-    const appID = req.body.appID
-    const appinfo = await appParser.getAppInfo(appID)
-    res.send(appinfo)
-})
+app.get("/get-app", async (req, res) => {
+	const appID = req.body.appID;
+	const appinfo = await appParser.getAppInfo(appID);
+	res.send(appinfo);
+});
 
-app.get('/get-category', async (req, res) => {
-    const name = req.body.category;
-    const category = await services.getCategoryFromDB(name);
-    res.send(category);
-})
+app.get("/get-category", async (req, res) => {
+	const name = req.body.category;
+	const category = await services.getCategoryFromDB(name);
+	res.send(category);
+});
 
-app.get('/get-category-list', async (req, res) => {
-    const categories = await services.getCategoryListFromDB()
-    res.send(categories)
-})
+app.get("/get-category-list", async (req, res) => {
+	const categories = await services.getCategoryListFromDB();
+	res.send(categories);
+});
 
-app.get('/search-app', async (req, res) => {
-    const query = req.body.query
-    const results = await services.searchApp(query)
-    res.send(results)
-})
+app.get("/search-app", async (req, res) => {
+	const query = req.body.query;
+	const results = await services.searchApp(query);
+	res.send(results);
+});
