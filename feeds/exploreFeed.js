7b44d6571858c59eab4e18eea63e48e7704315e9
Rajdeeep Malakar <rajdeep@zype.cf>
2022-04-17 17:16:58 +0530

    Fixed some issues related to explore feed and category list

--- feeds/exploreFeed.js
+++ feeds/exploreFeed.js
@@ -2,60 +2,61 @@ const { database, COLL_ID } = require("../constants");
 const { getAppInfo } = require("../parsers/appParser");
 
 async function getExploreFeed() {
-	let latestApps = [];
-	let data = await database.listDocuments(COLL_ID, [], 51, 0, undefined, undefined, ["uploadDate"], ["DESC"]);
-	for (app in data.documents) {
-		let appData = data.documents[app];
-		delete appData['$id'];
-		delete appData['$read'];
-		delete appData['$write'];
-		delete appData['$internalId'];
-		delete appData['$collection'];
-		latestApps.push(appData);
-	}
-	return latestApps;
+    let latestApps = [];
+    let data = await database.listDocuments(COLL_ID, [], 51, 0, undefined, undefined, ["uploadDate"], ["DESC"]);
+    for (app in data.documents) {
+        let appData = data.documents[app];
+        delete appData['$id'];
+        delete appData['$read'];
+        delete appData['$write'];
+        delete appData['$internalId'];
+        delete appData['$collection'];
+        appData['logo'] = "https://api.electron-store.org/app-img/" + appData['id'];
+        latestApps.push(appData);
+    }
+    return latestApps;
 }
 
 async function addExploreFeedCategory(category) {
-	const { data, error } = await supabase
-		.from("Explore")
-		.insert([{ title: category }]);
+    const { data, error } = await supabase
+        .from("Explore")
+        .insert([{ title: category }]);
 
-	if (error) {
-		return { error: error };
-	} else {
-		return { success: data };
-	}
+    if (error) {
+        return { error: error };
+    } else {
+        return { success: data };
+    }
 }
 
 async function addAppToExploreFeed(categoryTitle, appLink) {
-	const appID = appLink.replace("https://api.electron-store.org/apps/", "");
-	const appInfo = await getAppInfo(appID);
+    const appID = appLink.replace("https://api.electron-store.org/apps/", "");
+    const appInfo = await getAppInfo(appID);
 
-	let { data, error } = await supabase
-		.from("Explore")
-		.select("apps")
-		.eq("title", categoryTitle);
-	if (error) {
-		return { error: error };
-	}
-	let apps = [];
-	if (data[0] && data[0].apps) apps = data[0].apps;
-	const { data: dt, error: err } = await supabase
-		.from("Explore")
-		.insert([{ title: categoryTitle, apps: [...apps, appInfo] }], {
-			upsert: true,
-		});
+    let { data, error } = await supabase
+        .from("Explore")
+        .select("apps")
+        .eq("title", categoryTitle);
+    if (error) {
+        return { error: error };
+    }
+    let apps = [];
+    if (data[0] && data[0].apps) apps = data[0].apps;
+    const { data: dt, error: err } = await supabase
+        .from("Explore")
+        .insert([{ title: categoryTitle, apps: [...apps, appInfo] }], {
+            upsert: true,
+        });
 
-	if (err) {
-		return { error: err };
-	} else {
-		return { success: dt };
-	}
+    if (err) {
+        return { error: err };
+    } else {
+        return { success: dt };
+    }
 }
 
 module.exports = {
-	getExploreFeed,
-	addAppToExploreFeed,
-	addExploreFeedCategory,
+    getExploreFeed,
+    addAppToExploreFeed,
+    addExploreFeedCategory,
 };
--- parsers/categoryParser.js
+++ parsers/categoryParser.js
@@ -17,137 +17,138 @@ const yaml = require('js-yaml');
 // }
 
 const BASE_IMAGE_URL =
-	"https://pxfnmafyqvdhzxosxcqw.supabase.io/storage/v1/object/public/images/icons/";
+    "https://pxfnmafyqvdhzxosxcqw.supabase.io/storage/v1/object/public/images/icons/";
 
 async function getCategories() {
-	try {
-		const categories = await axios({url: "https://raw.githubusercontent.com/electron/apps/master/meta/categories.json", method: "GET"}).data;
-		let categoryNames = [];
-		for (let category in categories) {
-			categoryNames.push(category.name);
-		}
-		return categoryNames;
-	} catch (err) {
-		console.log(err);
-	}
+    try {
+        const categoryRAW = await axios({ url: "https://raw.githubusercontent.com/electron/apps/master/meta/categories.json", method: "GET" });
+        const categories = categoryRAW.data;
+        let categoryNames = [];
+        for (let category in categories) {
+            categoryNames.push(categories[category].name);
+        }
+        return categoryNames;
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 async function getApps() {
-	try {
-		// const categories = await getCategories();
-		// categories.forEach(async (category) => {await getCategoryApps(category)});
-		return await getCategoryApps();
-		// console.log(categories.length)
-		// console.log(await getCategoryApps(categories))
-	} catch (err) {
-		console.log(err)
-	}
+    try {
+        // const categories = await getCategories();
+        // categories.forEach(async (category) => {await getCategoryApps(category)});
+        return await getCategoryApps();
+        // console.log(categories.length)
+        // console.log(await getCategoryApps(categories))
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 async function getCategoryApps() {
-	try {
-		// // category = parser.parse(category)
-		// console.log(category)
-		// let html;
-		// html = await createRequest(
-		// 		`?category=${category.toLowerCase().replace(' & ', '-').replace(/\s/, "-")}`
-		// )
-		// const root = parser.parse(html);
-		// const listedApps = root.querySelectorAll(".listed-app");
-		// return listedApps.forEach(async (app) => {
-		// 	// console.log(app)
-		// 	const name = app.querySelector(".listed-app-name").textContent;
-		// 	const id = app
-		// 		.querySelector("a")
-		// 		.getAttribute("href")
-		// 		.replace("/apps/", "");
-		// 	const description = app.querySelector(
-		// 		".listed-app-description"
-		// 	).textContent;
-		// 	let IMG_URL = "https://api.electron-store.org" + app.querySelector(".listed-app-logo").getAttribute("data-src");
-		// 	toBase64(IMG_URL, (data) => {
-		// 		IMG_URL = data
-		// 	})
-		// 	const logo = IMG_URL;
-		// 	const uploadDate =
-		// 		app.querySelector(".listed-app-add-date")?.querySelector("span")
-		// 			?.textContent || "";
-		// 	const keywords =
-		// 		app.querySelector(".listed-app-keywords")?.textContent.trim() || "";
+    try {
+        // // category = parser.parse(category)
+        // console.log(category)
+        // let html;
+        // html = await createRequest(
+        // 		`?category=${category.toLowerCase().replace(' & ', '-').replace(/\s/, "-")}`
+        // )
+        // const root = parser.parse(html);
+        // const listedApps = root.querySelectorAll(".listed-app");
+        // return listedApps.forEach(async (app) => {
+        // 	// console.log(app)
+        // 	const name = app.querySelector(".listed-app-name").textContent;
+        // 	const id = app
+        // 		.querySelector("a")
+        // 		.getAttribute("href")
+        // 		.replace("/apps/", "");
+        // 	const description = app.querySelector(
+        // 		".listed-app-description"
+        // 	).textContent;
+        // 	let IMG_URL = "https://api.electron-store.org" + app.querySelector(".listed-app-logo").getAttribute("data-src");
+        // 	toBase64(IMG_URL, (data) => {
+        // 		IMG_URL = data
+        // 	})
+        // 	const logo = IMG_URL;
+        // 	const uploadDate =
+        // 		app.querySelector(".listed-app-add-date")?.querySelector("span")
+        // 			?.textContent || "";
+        // 	const keywords =
+        // 		app.querySelector(".listed-app-keywords")?.textContent.trim() || "";
 
-		// 	const appInfo = {
-		// 		id,
-		// 		name,
-		// 		description,
-		// 		logo,
-		// 		uploadDate,
-		// 		keywords,
-		// 		category,
-		// 		developer: 'unclaimedapps@electron-store.org'
-		// 	};
-		// 	return await pushAppToDB(appInfo);
-		// });
-		const appsNameList = await getAppList();
-		// const releases = await axios({url: "https://raw.githubusercontent.com/electron/apps/master/meta/releases.json", method: "GET"});
-		const dates = await axios({url: "https://raw.githubusercontent.com/electron/apps/master/meta/dates.json", method: "GET"});
-		return appsNameList.forEach(async (appName) => {
-			try {
-			// console.log(appName)
-			const res = await axios({url: `https://raw.githubusercontent.com/electron/apps/master/apps/${appName}/${appName}.yml`, method: "GET"})
-			if (res.status == 200) {
-				const appYAML = res.data;
-				const appData = yaml.load(appYAML)
-				const app = {
-					id: appName,
-					name: appData.name,
-					description: appData.description,
-					keywords: appData.keywords ? appData.keywords.join(',') : '',
-					repo: appData.repository,
-					website: appData.website,
-					category: appData.category,
-					license: appData.license,
-					developer: "electron-store@oneid",
-					claimed: false,
-					uploadDate: dates.data[appName]
-				}
-				return await require('../feeds/appFeed').pushAppToDB(app);
-			}
-		} catch (err) {
-			if (err.response) {
-				// Axios Request Error
-				if (err.response.status == 404) {
-					// Electron Apps repo bug
-				} else {
-					// Actual error
-					console.log(err.response.status);
-				}
-			} else {
-				// Something else happened while requesting
-				console.log(err);
-			}
-		}
-		})
-	} catch (err) {
-		console.log(err)
-	}
+        // 	const appInfo = {
+        // 		id,
+        // 		name,
+        // 		description,
+        // 		logo,
+        // 		uploadDate,
+        // 		keywords,
+        // 		category,
+        // 		developer: 'unclaimedapps@electron-store.org'
+        // 	};
+        // 	return await pushAppToDB(appInfo);
+        // });
+        const appsNameList = await getAppList();
+        // const releases = await axios({url: "https://raw.githubusercontent.com/electron/apps/master/meta/releases.json", method: "GET"});
+        const dates = await axios({ url: "https://raw.githubusercontent.com/electron/apps/master/meta/dates.json", method: "GET" });
+        return appsNameList.forEach(async(appName) => {
+            try {
+                // console.log(appName)
+                const res = await axios({ url: `https://raw.githubusercontent.com/electron/apps/master/apps/${appName}/${appName}.yml`, method: "GET" })
+                if (res.status == 200) {
+                    const appYAML = res.data;
+                    const appData = yaml.load(appYAML)
+                    const app = {
+                        id: appName,
+                        name: appData.name,
+                        description: appData.description,
+                        keywords: appData.keywords ? appData.keywords.join(',') : '',
+                        repo: appData.repository,
+                        website: appData.website,
+                        category: appData.category,
+                        license: appData.license,
+                        developer: "electron-store@oneid",
+                        claimed: false,
+                        uploadDate: dates.data[appName]
+                    }
+                    return await require('../feeds/appFeed').pushAppToDB(app);
+                }
+            } catch (err) {
+                if (err.response) {
+                    // Axios Request Error
+                    if (err.response.status == 404) {
+                        // Electron Apps repo bug
+                    } else {
+                        // Actual error
+                        console.log(err.response.status);
+                    }
+                } else {
+                    // Something else happened while requesting
+                    console.log(err);
+                }
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 async function getAppList() {
-	try {
-		const res = await axios({url: 'https://raw.githubusercontent.com/electron/apps/master/meta/dates.json', method: "GET"});
-		const numberOfApps = Object.keys(res.data);
-		// console.log(numberOfApps.length)
-		return numberOfApps;
-		// console.log(res.data)
-	} catch (err) {
-		console.log(err);
-	}
+    try {
+        const res = await axios({ url: 'https://raw.githubusercontent.com/electron/apps/master/meta/dates.json', method: "GET" });
+        const numberOfApps = Object.keys(res.data);
+        // console.log(numberOfApps.length)
+        return numberOfApps;
+        // console.log(res.data)
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = {
-	getApps,
-	getAppList,
-	getCategories,
-	getCategoryApps,
-	BASE_IMAGE_URL,
+    getApps,
+    getAppList,
+    getCategories,
+    getCategoryApps,
+    BASE_IMAGE_URL,
 };
